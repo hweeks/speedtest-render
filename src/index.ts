26ce3f3c2ab6e745dff3a8ef17620fb54028277c
ha-m1-top-buddi <idareutoemailme@hweeks.com>
2022-11-23 17:41:59 -0700

    feat: add basic api

--- src/index.ts
+++ src/index.ts
@@ -27,16 +27,17 @@ export const figure_my_speed_out_then_save_it_to_disk = () => {
   writeFileSync(join(file_path, filename), JSON.stringify(the_result, null, 2));
 };
 
-export const gimmie_that_json = () => {
+export const gimmie_that_json = (filter?: string) => {
   const file_path = resolve(__dirname, "../data");
   const cmd_to_run = execSync("ls", {
     cwd: file_path,
   });
-  const files_to_read = cmd_to_run.toString().split("\n").filter(Boolean);
+  const files_to_read = cmd_to_run.toString().split("\n").filter(Boolean); // get each listed file
   const all_values: speed_result[] = [];
   files_to_read.forEach((path_to_json) => {
     const full_json_path = resolve(file_path, path_to_json);
-    all_values.push(JSON.parse(readFileSync(full_json_path, "utf-8")));
+    let should_push = (filter && !path_to_json.includes(filter)) ?? true
+    if (should_push) all_values.push(JSON.parse(readFileSync(full_json_path, "utf-8")));
   });
   return all_values;
 };
--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ app.use(express.json());
 app.engine('html', (filePath: string, options: {}, callback: any) => { // define the template engine
   readFile(filePath, (err, content) => {
     if (err) return callback(err)
-    debugger
     const rendered = content.toString()
       .replace('{{ DATA_FROM_SERVER }}', `${(options as any).data.map(datum => JSON.stringify(datum)).join(',')}`)
     return callback(null, rendered)
@@ -23,17 +22,23 @@ app.set('view engine', 'html')
 
 app.get("/", (req, res) => {
   const all_data = gimmie_that_json();
-  debugger
-  res.render("index", {data: all_data}, (err: Error, html: string) => {
-    res.send(html)
-  });
+  res.render("index", {data: all_data});
+});
+
+app.get("/api/results", (req, res) => {
+  const all_data = gimmie_that_json();
+  res.send(all_data)
+});
+
+app.get("/api/results/:timestamp", (req, res) => {
+  const all_data = gimmie_that_json(req.params.timestamp);
+  res.send(all_data[0])
 });
 
 app.listen(8080, () => {
-  console.log("up")
-  figure_my_speed_out_then_save_it_to_disk()
-  console.log("latest speed written")
-  cron.schedule('*/15 * * * *', () => {
+  console.log("up, will take five minutes to populate the first results")
+  cron.schedule('*/5 * * * *', () => {
+    console.log("running speed test")
     figure_my_speed_out_then_save_it_to_disk()
     console.log("latest speed written")
   });
