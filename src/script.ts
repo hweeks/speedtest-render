096ae659345ef94e05d28250a9c39851eeafbce9
ha-m1-top-buddi <idareutoemailme@hweeks.com>
2022-11-24 08:52:44 -0700

    feat: adding better formatting

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,3 @@
-import {figure_my_speed_out_then_save_it_to_disk} from './index'
+import { figure_my_speed_out_then_save_it_to_disk } from "./index";
 
-figure_my_speed_out_then_save_it_to_disk()
+figure_my_speed_out_then_save_it_to_disk();
